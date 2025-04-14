46d9c6fcbf20415748ed81876f8d6b2675d43375
Lakshay Neem <124112721+lakshayneem@users.noreply.github.com>
2025-04-15 00:31:29 +0530

    flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -189,13 +189,7 @@ def analyze_review():
 
 
 
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
