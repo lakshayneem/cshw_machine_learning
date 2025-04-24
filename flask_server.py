2fcccd02fcabb85e03005b5b9680dc53ddb05d63
Lakshay Neem <124112721+lakshayneem@users.noreply.github.com>
2025-04-25 01:55:13 +0530

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -13,7 +13,7 @@ from nltk.stem import WordNetLemmatizer
 
 # Flask app initialization
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://cshw-frontend-fake-job-detection.onrender.com"])
 
 # Download necessary NLTK data
 nltk.download('stopwords')
